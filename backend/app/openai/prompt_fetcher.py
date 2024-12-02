3ad7ca73d70d8b85e8a174131166d879fee0dde4
Muneeb Rasool <rasoom@me.com>
2024-12-02 15:51:11 -0800

    Update prompt_fetcher.py

--- backend/app/openai/prompt_fetcher.py
+++ backend/app/openai/prompt_fetcher.py
@@ -1,7 +1,7 @@
 import requests
 class PromptFetcher:
   def __init__(self):
-    self.api_key = "$2a$10$No8dXG4S5NVTsfmN3otgnueEgWvlAhdeDDWFaIubT5EwXr9WVL0Z2"
+    self.api_key = "API-KEY"
   def fetch_recommendations(self):
     return requests.get('https://api.jsonbin.io/v3/b/6597b87d1f5677401f17c0af', headers= { "X-Master-Key" :self.api_key }).json()["record"]
 
