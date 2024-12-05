c5267f034c6bc60a169a4a74d97dd118b434d320
Paul Pfeister <code@pfeister.dev>
2024-12-04 20:34:09 -0500

    fix: tailwind

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,23 +1,24 @@
 import StytchProvider from "./components/StytchProvider"
-import { MantineProvider, AppShell } from "@mantine/core"
+import { MantineProvider } from "@mantine/core"
+import LayoutWrapper from "./components/LayoutWrapper"
+
+import "./globals.css"
 
 export const metadata = {
     title: 'Iterative',
     description: 'AI-Powered React Code Generator',
   }
   
-  export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
+  export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <MantineProvider withGlobalStyles withNormalizeCSS>
-          <AppShell>
-            <StytchProvider children={children} >
-              {children}
-            </StytchProvider>
-          </AppShell>
+      <StytchProvider children>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+        >
+          <LayoutWrapper children>
+          </LayoutWrapper>
         </MantineProvider>
+      </StytchProvider>
     )
   }
